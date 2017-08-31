9023637f1de1f6905fa2c6d142fb28fa3518af10
joseildo <joseildofilho@yahoo.com.br>
2017-08-31 18:19:01 -0300

    added heap structure

---
+++ Heap.c
@@ -0,0 +1,38 @@
+﻿#include "Heap.h"
+#include <stdlib.h>
+Heap construcHeap(int size,enum Type t) 
+{
+	Heap h;
+	h.size = size;
+	h.count = 0;
+	h.heap = malloc(sizeof(int)*size);
+	h.t = t;
+	return h;
+};
+void insert(Heap h, int value) 
+{
+	int i = 1,j = 1;
+	if(!h.count) {
+		h.heap[1] = value;
+	}
+	while(h.heap[i] && h.heap[i+1]) {
+		i = 2 * j;
+		if (i < h.size) {
+			int * x = malloc(2*sizeof(int)*h.size);
+			int j;
+			for(j = 0; j < h.size; j++) {
+				x[j] = h.heap[j];
+			}
+			free(h.heap);
+			h.heap = x;
+			h.size *= 2;
+		}
+		j++;
+	}
+	if(!i) {
+		h.heap[i] = value;
+	} else {
+		h.heap[i+1] = value;
+	}
+	h.count++;
+}
---
+++ Heap.h
@@ -0,0 +1,10 @@
+﻿#ifndef HEAP_H
+#define HEAP_H
+typedef enum Type {MAX, MIN} Type;
+typedef struct Heap {
+	int size,* heap, count;
+	Type t;
+} Heap;
+extern Heap makeHeap(int size,Type t);
+extern void insert(Heap h, int value);
+#endif
