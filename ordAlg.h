45d39148445d9edf96e50de389c82f3452948a59
joseildofilho <joseildofilho@yahoo.com.br>
2017-09-11 20:08:20 -0300

    altering to get better

--- ordAlg.h
+++ ordAlg.h
@@ -4,7 +4,7 @@ void selection(int *list,int size);
 void insertion(int *list,int size);
 void mergeSort(int *list,int * aux, int start, int end);
 void quickSort(int *list,int start, int end);
-void count(int *list, int size);
+void count(int **list, int size);
 void bucket(int *list, int size);
-void radix(int *list, int size);
+void radix(int **list, int size);
 #endif
